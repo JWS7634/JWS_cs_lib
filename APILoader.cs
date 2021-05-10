9bc9a8ee22375bfe3b21ae1bb89726edee77abf1
JWS <window2254@naver.com>
2021-05-10 21:24:29 +0900

    Create APILoader.cs

---
+++ APILoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace JPLI
+{
+	class JPLoader
+	{
+		JObject root;
+		public void APILoad(string reference)
+		{
+			root = JObject.Parse(new WebClient().DownloadString(reference));
+		}
+		public string GetData(string key)
+		{
+			if (root.ContainsKey(key))
+				return root[key].ToString();
+			else
+				return null;
+		}
+	}
+}
